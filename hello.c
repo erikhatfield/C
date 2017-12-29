cd9d111685586db0480703eed522ad6badaebb3b
erikhatfield <me@erikhatfield>
2017-12-28 17:03:17 -0800

    Hello World Commit

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void)
+{
+  // Since this 'hello' program is for my Git hub, perhaps 'Hello Internet' is most appropriate :)
+  printf("Hello Internet!\n");
+  return 0;
+}
